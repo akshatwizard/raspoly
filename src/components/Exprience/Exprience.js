a2855d363ea8829384395b081b13c0d7567e155e
PriyeshRai369 <priyeshrai369@gmail.com>
2024-09-19 11:44:39 +0530

    New Changes done.

--- src/components/Exprience/Exprience.js
+++ src/components/Exprience/Exprience.js
@@ -1,27 +1,31 @@
 import React from "react";
 
 export default function Exprience() {
-
   return (
     <section className=" mt-5 py-5 expContainer">
       <div className="container">
-        <div className="row">
-          <div className="col-lg-3 col-md-3 col-6 col-xs-12 text-center contentBox my-2">
-            <h1 className="year">40 <sup>+</sup> </h1>
+        <div className="row row-gap-4">
+          <div className="col-lg-4 col-md-6 col-12 col-xs-12 text-center contentBox my-2">
+            <h1 className="year">40+</h1>
             <p>Years of Experience</p>
           </div>
-          <div className="col-lg-3 col-md-3 col-6 col-xs-12 text-center contentBox my-2">
+          <div className="col-lg-4 col-md-6 col-12 col-xs-12 text-center contentBox my-2">
+            <h1 className="year">40K+</h1>
+            <p> Metric Tons</p>
+          </div>
+          <div className="col-lg-4 col-md-6 col-12 col-xs-12 text-center contentBox my-2">
             <h1>4mW</h1>
             <p>Of captive solar energy production</p>
           </div>
-          <div className="col-lg-3 col-md-3 col-6 col-xs-12 text-center contentBox my-2">
+          <div className="col-lg-4 col-md-6 col-12 col-xs-12 text-center contentBox my-2">
             <h1> 5 </h1>
-            <p>Presence across continents </p>
+            <p>Presence in continents </p>
           </div>
-          <div className="col-lg-3 col-md-3 col-6 col-xs-12 text-center contentBox my-2">
-          <h1 className="year">40K <sup>+</sup> </h1>
-            <p> Metric Tons</p>
+          <div className="col-lg-4 col-md-6 col-12 col-xs-12 text-center contentBox my-2">
+            <h1> 5 </h1>
+            <p>Presence in locations of India </p>
           </div>
+          
         </div>
       </div>
     </section>
