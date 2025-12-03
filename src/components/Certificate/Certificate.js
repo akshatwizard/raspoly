c33d405c1a5ac4b4beed816b0ab20b51c92deed8
akshatwizard <akshat@wizards.co.in>
2025-12-03 13:40:33 +0530

    Update Certificate.js

--- src/components/Certificate/Certificate.js
+++ src/components/Certificate/Certificate.js
@@ -8,9 +8,11 @@ export default function Certificate() {
           <div className="section-title-wrap without-tab mb-5">
             <h2 className="section-title mb-3">Certificates and Memberships</h2>
           </div>
+    {/*
           <div className="col-lg-3 col-md-4 col-sm-6 col-6">
             <img src="/assets/images/certificate/raspoly-aenor.jpeg" alt="" />
           </div>
+          */}
           <div className="col-lg-3 col-md-4 col-sm-6 col-6">
             <img src="/assets/images/certificate/raspoly-iso9001.jpg" alt="" />
           </div>
